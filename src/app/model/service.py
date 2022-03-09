2cf53a7f19c717f54f4d2e20df94d83ed2e54af7
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-03-09 16:01:36 -0600

    Actualizado - Sin cambios

--- src/app/model/service.py
+++ src/app/model/service.py
@@ -10,7 +10,14 @@ if __name__ == '__main__':
     print("This is not an usable Python script.\nUsage: python3 weather_app.py\n")
     os._exit(os.EX_OK)
 
-def get_weather(lat, lon):
-    payload = {'lat': lat, 'lon': lon, 'appid': API_KEY}
+def get_json_info(lat, lon, units='metric'):
+    payload = {'lat': lat, 'lon': lon, 'units': units, 'appid': API_KEY}
     r = requests.get(API_DOMAIN, params=payload)
-    print(r.json());
+    ## Checamos que la URL este correcta.
+    assert( r.url == f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units={units}&appid={API_KEY}" )
+
+    ## Ahora probamos que la respuesta del servidor sea exitosa.
+    assert( r.status_code == 200 )
+
+    ## Para este punto ya tenemos nuestro JSON con la temperatura requerida en Kelvin
+    return r.json()
