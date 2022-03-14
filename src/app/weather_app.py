2e80fe97d59c1a519822371c72e3f28a9a253464
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2022-03-13 19:04:08 -0600

    Abstraida logica de peticiones, el cache ya funciona, anadida creacion de reportes

--- src/app/weather_app.py
+++ src/app/weather_app.py
@@ -1,8 +1,10 @@
 import model.service as service
 import model.cache_processor as cache
 import model.dict_creator as dc
+import model.report_generator as report
 import csv
-import time
+from time import sleep
+
 
 # Diccionario donde guardamos nuestras peticiones ya procesadas, y el cual usaremos
 # para mostrar nuestros datos.
@@ -21,20 +23,40 @@ def main():
             if line_count == 0:
                 line_count += 1
             else:
-                if line_count == 5:
+                if line_count == 100:
                     break
                 # Sabemos que las columnas 2,3 son las coordenadas de nuestro aeropuerto de origen
                 # mientras que las columnas 4,5 son las coordenadas del aeropuerto destino.
                 # Estas columnas las obtenemos con row[i].
-                airport_origin = service.get_json_info(row[2],row[3])
-                airport_cache[row[0]] = dc.create_dict(airport_origin, row[0])
-                airport_dest = service.get_json_info(row[4],row[5])
-                airport_cache[row[1]] = dc.create_dict(airport_dest, row[1])
 
-                print(airport_cache)
-
+                process_data(row)
                 line_count += 1
+    report.generate(airport_cache)
     cache.write_to_cache(airport_cache)
+
+def is_in_cache(iata):
+    if iata in airport_cache:
+        return True
+    return False
+
+def process_airport(row, dest_origin_flag):
+    iata = row[dest_origin_flag]
+    if dest_origin_flag == 0:
+        i = 2
+    else: 
+        i = 4
+
+    if not is_in_cache(iata):
+        airport_origin = service.get_json_info(row[i], row[i+1])
+        airport_cache[iata] = dc.create_dict(airport_origin, iata)
+    else:
+        if cache.is_cache_expired(airport_cache[iata]["time_of_consult"]):
+            airport_origin = service.get_json_info(row[i], row[i+1])
+            airport_cache[iata] = dc.create_dict(airport_origin, iata)
+
+def process_data(row):
+    process_airport(row, 0)
+    process_airport(row, 1)
 
 if __name__ == '__main__':
     main()
