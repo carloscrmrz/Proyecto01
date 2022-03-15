141f1207394e9d344574b872e9cab4c5c4013eb0
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2022-03-14 20:59:09 -0600

    Agregado formato al HTML para vista mas limpia

--- src/app/model/report_generator.py
+++ src/app/model/report_generator.py
@@ -7,13 +7,13 @@ def generate(cache):
     <meta charset="utf-8">
     <body>
     <table>
-    <tr>
-    <th>Aeropuerto</th>
-    <th>Temperatura</th>
-    <th>Temp. Min.</th>
-    <th>Temp. Max.</th>
-    <th>Descripcion</th>
-    </tr>
+    <tr>\n
+    \t<th>Aeropuerto</th>\n
+    \t<th>Temperatura</th>\n
+    \t<th>Temp. Min.</th>\n
+    \t<th>Temp. Max.</th>\n
+    \t<th>Descripcion</th>\n
+    </tr>\n
     """
     for report in cache:
         html += create_table_row(cache[report])
@@ -25,12 +25,12 @@ def generate(cache):
         f.close()
 
 def create_table_row(airport_dict):
-    table = "<tr>"
-    table += f"<th>{airport_dict['name']}</th>"
-    table += f"<th>{airport_dict['temp']}°C</th>"
-    table += f"<th>{airport_dict['temp_min']}°C</th>"
-    table += f"<th>{airport_dict['temp_max']}°C</th>"
-    table += f"<th>{airport_dict['weather']}</th>"
-    table += "</tr>"
+    table = "<tr>\n"
+    table += f"\t<th>{airport_dict['name']}</th>\n"
+    table += f"\t<th>{airport_dict['temp']}°C</th>\n"
+    table += f"\t<th>{airport_dict['temp_min']}°C</th>\n"
+    table += f"\t<th>{airport_dict['temp_max']}°C</th>\n"
+    table += f"\t<th>{airport_dict['weather']}</th>\n"
+    table += "</tr>\n"
 
     return table
