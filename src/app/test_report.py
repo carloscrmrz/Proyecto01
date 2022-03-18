ebe152dc98f9b0a37a986cacc37dca3aa94cc1bc
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2022-03-18 14:06:53 -0600

    Cambiada la prueba unitaria para el cambio de la tabla en el reporte

--- src/app/test_report.py
+++ src/app/test_report.py
@@ -7,12 +7,10 @@ def test_create_table_row():
     """
     sanitized_data = {'name': "MEX", 'time_of_consult': datetime.now().strftime("%Y-%m-%d %H:%M"), 'weather': 'soleado', 'temp': 23, 'temp_min': 16, 'temp_max': 31, 'feels_like': 24}
     
-    table = "<tr>\n"
-    table += "\t<th>MEX</th>\n"
+    table = "\t<th>MEX</th>\n"
     table += "\t<th>23°C</th>\n"
     table += "\t<th>16°C</th>\n"
     table += "\t<th>31°C</th>\n"
     table += "\t<th>soleado</th>\n"
-    table += "</tr>\n"
 
     assert report.create_table_row(sanitized_data) == table
